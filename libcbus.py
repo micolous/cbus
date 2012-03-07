e543fc22f3ee690dcf8d69da10cd6332ce68337c
Michael Farrell <micolous+git@gmail.com>
2012-03-07 21:55:56 +1030

    victory.  now python can talk to cbus.  helps when my panel is programmed with group addresses

--- libcbus.py
+++ libcbus.py
@@ -2,8 +2,10 @@
 import struct
 from serial import Serial
 
+END_COMMAND = '\r\n'
+
 # command types
-POINT_TO_MULTIPOINT = '\\05'
+POINT_TO_MULTIPOINT = '~\\05'
 
 # Applications
 APP_LIGHTING = '38'
@@ -11,7 +13,8 @@ APP_LIGHTING = '38'
 # Routing buffer
 ROUTING_NONE = '00'
 
-
+LIGHT_ON = '79'
+LIGHT_OFF = '01'
 # light on
 #\0538007964 (GA 100)
 
@@ -20,6 +23,26 @@ ROUTING_NONE = '00'
 
 # set to level
 #\053800rr64FF (GA 100, to level 100%/0xff)
+
+RAMP_RATES = {
+	'02': 0,
+	'0A': 4,
+	'12': 8,
+	'1A': 12,
+	'22': 20,
+	'2A': 30,
+	'32': 40,
+	'3A': 60,
+	'42': 90,
+	'4A': 120,
+	'52': 180,
+	'5A': 300,
+	'62': 420,
+	'6A': 600,
+	'72': 900,
+	'7A': 1020
+}
+
 
 def duration_to_ramp_rate(seconds):
 	if seconds == 0:
@@ -56,6 +79,22 @@ def duration_to_ramp_rate(seconds):
 		return '7A'
 	raise OutOfRangeException, 'That duration is too long'
 
+def ramp_rate_to_duration(rate):
+	assert len(rate) == 2, "Ramp rate must be two characters."
+	rate = rate.upper()	
+	return RAMP_RATES[rate]
+
+def cbus_checksum(input):
+	"Calculates the checksum of a C-Bus command string."
+	if input[0] == '\\':
+		input = input[1:]
+		
+	input = input.decode('base16')
+	c = 0
+	for x in input:
+		c += ord(x)
+	
+	return ((c % 256) ^ 256) + 1
 
 class CBusPCISerial(object):
 	def __init__(self, device):
@@ -63,8 +102,18 @@ class CBusPCISerial(object):
 		self.reset()
 
 	def reset(self):
-		# reset the PCI
-		self.s.write('~')
+		# reset the PCI, enable MMI reports so we know when buttons are pressed.
+		# (mmi is actually disabled, 59g vs 79g
+		self.write('~~~\r\nA3210038g\r\nA3420002g\r\nA3300059g\r\n')
 	
+	def write(self, msg):
+		print "Message = %r" % msg
+		self.s.write(msg)
 	
-
+	def lighting_group_on(self, group_id):
+		# TODO: Implement checksumming
+		self.write(POINT_TO_MULTIPOINT + APP_LIGHTING + ROUTING_NONE + LIGHT_ON + ('%02X' % group_id) + 'g' + END_COMMAND)
+	
+	def lighting_group_off(self, group_id):
+		# TODO: Implement checksumming
+		self.write(POINT_TO_MULTIPOINT + APP_LIGHTING + ROUTING_NONE + LIGHT_OFF + ('%02X' % group_id) + 'g' + END_COMMAND)
