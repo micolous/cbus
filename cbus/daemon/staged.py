71e3176722836448c6be94db119b2e8244f6cbcc
Michael Farrell <micolous+git@gmail.com>
2012-05-24 00:08:00 +0930

    started planning the `staged` daemon.

---
+++ cbus/daemon/staged.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# staged: "simple" daemon for creating custom scenes listening on lighting GAs
+# Copyright 2012 Michael Farrell <micolous+git@gmail.com>
+# 
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+`staged` allows you to create simple scenes using INI files that listen for
+lighting group address messages and transmits additional messages via cdbusd.
+
+"""
+
+DEFAULT_CONFIG_FILE = '/etc/cbus/staged.ini'
+
