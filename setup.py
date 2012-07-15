32102bb7fdca6208867f5d68f29e784fabb70707
Michael Farrell <micolous+git@gmail.com>
2012-07-15 22:01:50 +0930

    fix issues with setup.py having a stuffed dependancy list

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from platform import system
+system = system().lower()
+
+deps = [
+		'configparser_plus (>=1.0)',
+		'serial (>=2.6)',
+		'lxml (>=2.3.2)',
+		'Twisted (>=12.0.0)',
+]
+
+if system != 'windows':
+	# Windows doesn't support some of these things so we run it in reduced
+	# functionality mode.
+	deps += [
+		'daemon (>=1.0)',
+		'dbus (>=1.1.1)',
+		'pygobject (>=2.28.6)'
+	]
+
 
 setup(
 	name="cbus",
@@ -10,19 +29,7 @@ setup(
 	author_email="micolous@gmail.com",
 	url="https://github.com/micolous/cbus",
 	license="LGPL3+",
-	requires=(
-		'configparser_plus (>=1.0)',
-		'serial (>=2.6)',
-		'lxml',
-		'Twisted (>=12.0.0)',
-		'PyGObject',
-		'dbus-python',
-		
-		# Extra optional dependancies not listed here:
-		# cbusbot requires ircasync, from https://github.com/micolous/ircbots
-		# cdbusd requires python-dbus; only works on UNIX systems.
-	),
-	
+	requires=deps,
 	
 	# TODO: add scripts to this.
 	packages=find_packages(),
