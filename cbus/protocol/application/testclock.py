bc5227c0aec640db3f533fd0f5abd3f75606a295
Michael Farrell <micolous+git@gmail.com>
2012-08-23 23:53:11 +0930

    Disable travis python 2.5 support.  Migrated all tests to nose, now they should work on Python 2.6

--- cbus/protocol/application/testclock.py
+++ cbus/protocol/application/testclock.py
@@ -15,77 +15,72 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
 from cbus.protocol.packet import decode_packet
 from cbus.protocol.pm_packet import PointToMultipointPacket
 from cbus.protocol.application.clock import *
 from cbus.common import *
 from datetime import time, date
 
-class S23_13_1_Test(unittest.TestCase):
-	def runTest(self):
-		"Example in s23.13.1 of decoding a time"
-		# Set network time to 10:43:23 with no DST offset
-		# Slight change from guide:
-		p, r = decode_packet('\\05DF000D010A2B1700C2g', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], ClockUpdateSAL)
-		self.assertTrue(p.sal[0].is_time)
-		self.assertFalse(p.sal[0].is_date)
-		self.assertIsInstance(p.sal[0].val, time)
-		
-		self.assertEqual(p.sal[0].val.hour, 10)
-		self.assertEqual(p.sal[0].val.minute, 43)
-		self.assertEqual(p.sal[0].val.second, 23)
-		# Library doesn't handle DST offset, so this flag is dropped.
-		
-		## check that it encodes properly again
-		# fuzzy match to allow packet that has no DST information
-		self.assertIn(p.encode(), ['05DF000D010A2B1700C2', '05DF000D010A2B17FFC3'])
-		self.assertEqual(p.confirmation, 'g')
-		
-		
-class S23_13_2_Test(unittest.TestCase):
-	def runTest(self):
-		"Example in s23.13.2 of decoding a date"
-		# Set network date to 2005-02-25 (Friday)
-		p, r = decode_packet('\\05DF000E0207D502190411g', server_packet=False)
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], ClockUpdateSAL)
-		self.assertTrue(p.sal[0].is_date)
-		self.assertFalse(p.sal[0].is_time)
-		self.assertIsInstance(p.sal[0].val, date)
-		
-		self.assertEqual(p.sal[0].val.year, 2005)
-		self.assertEqual(p.sal[0].val.month, 2)
-		self.assertEqual(p.sal[0].val.day, 25)
-		self.assertEqual(p.sal[0].val.weekday(), 4) # friday
+def S23_13_1_Test():
+	"Example in s23.13.1 of decoding a time"
+	# Set network time to 10:43:23 with no DST offset
+	# Slight change from guide:
+	p, r = decode_packet('\\05DF000D010A2B1700C2g', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket), "Packet is not PointToMultipointPacket"
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], ClockUpdateSAL)
+	assert p.sal[0].is_time
+	assert not p.sal[0].is_date
+	assert isinstance(p.sal[0].val, time)
+	
+	assert p.sal[0].val.hour == 10
+	assert p.sal[0].val.minute == 43
+	assert p.sal[0].val.second == 23
+	
+	# Library doesn't handle DST offset, so this flag is dropped.
+	
+	## check that it encodes properly again
+	# fuzzy match to allow packet that has no DST information
+	assert p.encode() in ['05DF000D010A2B1700C2', '05DF000D010A2B17FFC3']
+	assert p.confirmation == 'g'
 
-		## check that it encodes properly again
-		self.assertIn(p.encode(), '05DF000E0207D502190411')
-		self.assertEqual(p.confirmation, 'g')
+def S23_13_2_Test():
+	"Example in s23.13.2 of decoding a date"
+	# Set network date to 2005-02-25 (Friday)
+	p, r = decode_packet('\\05DF000E0207D502190411g', server_packet=False)
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], ClockUpdateSAL)
+	assert p.sal[0].is_date
+	assert not p.sal[0].is_time
+	assert isinstance(p.sal[0].val, date)
+	
+	assert p.sal[0].val.year == 2005
+	assert p.sal[0].val.month == 2
+	assert p.sal[0].val.day == 25
+	assert p.sal[0].val.weekday() == 4 # friday
+
+	## check that it encodes properly again
+	assert p.encode() == '05DF000E0207D502190411'
+	assert p.confirmation == 'g'
 
 		
-class S23_13_3_Test(unittest.TestCase):
-	def runTest(self):
-		"Example in s23.13.3 of decoding request refresh"
-		# Request refreeh command
-		# documentation is wrong here:
-		#  - says      05DF00100C
-		#  - should be 05DF00110308
-		
-		p, r = decode_packet('\\05DF00110308g', server_packet=False)
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], ClockRequestSAL)
-		
-		## check that it encodes properly again
-		self.assertIn(p.encode(), '05DF00110308')
-		self.assertEqual(p.confirmation, 'g')
-
+def S23_13_3_Test():
+	"Example in s23.13.3 of decoding request refresh"
+	# Request refreeh command
+	# documentation is wrong here:
+	#  - says      05DF00100C
+	#  - should be 05DF00110308
+	
+	p, r = decode_packet('\\05DF00110308g', server_packet=False)
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], ClockRequestSAL)
+	
+	## check that it encodes properly again
+	assert p.encode() == '05DF00110308'
+	assert p.confirmation == 'g'
--- cbus/protocol/application/testlighting.py
+++ cbus/protocol/application/testlighting.py
@@ -15,132 +15,129 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
 from cbus.protocol.packet import decode_packet
 from cbus.protocol.pm_packet import PointToMultipointPacket
 from cbus.protocol.application.lighting import *
 from cbus.common import *
 
-class LightingSerialInterfaceGuideTests(unittest.TestCase):
-	def runTest(self):
-		"Examples in serial interface guide, s6.4"
-		# Switch on light at GA 8
-		p, r = decode_packet('\\0538000108BAg', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], LightingOffSAL)
-		self.assertEqual(p.sal[0].group_address, 8)
-		
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '0538000108BA')
-		self.assertEqual(p.confirmation, 'g')
-		
-		
-		p, r = decode_packet('\\05380001087909090A25h', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 3)
-		
-		# turn off light 8
-		self.assertIsInstance(p.sal[0], LightingOffSAL)
-		self.assertEqual(p.sal[0].group_address, 8)
-		
-		# turn on light 9		
-		self.assertIsInstance(p.sal[1], LightingOnSAL)
-		self.assertEqual(p.sal[1].group_address, 9)
-		
-		# terminate ramp on light 10)
-		self.assertIsInstance(p.sal[2], LightingTerminateRampSAL)
-		self.assertEqual(p.sal[2].group_address, 10)
-		
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '05380001087909090A25')
-		self.assertEqual(p.confirmation, 'h')
+def S6_4_Test():
+	"Examples in serial interface guide, s6.4"
+	# Switch on light at GA 8
+	p, r = decode_packet('\\0538000108BAg', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], LightingOffSAL)
+	assert p.sal[0].group_address == 8
+	
+	# check that it encodes properly again
+	assert p.encode() == '0538000108BA'
+	assert p.confirmation == 'g'
+	
+	# concatenated packet
+	p, r = decode_packet('\\05380001087909090A25h', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 3
+	
+	# turn off light 8
+	assert isinstance(p.sal[0], LightingOffSAL)
+	assert p.sal[0].group_address == 8
+	
+	# turn on light 9		
+	assert isinstance(p.sal[1], LightingOnSAL)
+	assert p.sal[1].group_address == 9
+	
+	# terminate ramp on light 10)
+	assert isinstance(p.sal[2], LightingTerminateRampSAL)
+	assert p.sal[2].group_address == 10
+	
+	# check that it encodes properly again
+	assert p.encode() == '05380001087909090A25'
+	assert p.confirmation == 'h'
 		
 
-class LightingApplicationTests(unittest.TestCase):
-	def runTest(self):
-		"Examples in Lighting Application s2.11"
-		# switch on light at GA 0x93
-		p, r = decode_packet('\\0538007993B7j', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], LightingOnSAL)
-		self.assertEqual(p.sal[0].group_address, 0x93)
-		
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '0538007993B7')
-		self.assertEqual(p.confirmation, 'j')
+def S2_11_Test():
+	"Examples in Lighting Application s2.11"
+	# switch on light at GA 0x93
+	p, r = decode_packet('\\0538007993B7j', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], LightingOnSAL)
+	assert p.sal[0].group_address == 0x93
+	
+	# check that it encodes properly again
+	assert p.encode() == '0538007993B7'
+	assert p.confirmation == 'j'
 
 
-class LightingQuickStartGuideTests(unittest.TestCase):
-	def runTest(self):
-		"Examples in quick start guide, s9.1"
-		# turn on light 0x21
-		p, r = decode_packet('\\053800792129i', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], LightingOnSAL)
-		self.assertEqual(p.sal[0].group_address, 0x21)
+def S9_1_Test():
+	"Examples in quick start guide, s9.1"
+	# turn on light 0x21
+	p, r = decode_packet('\\053800792129i', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal), 1
+	
+	assert isinstance(p.sal[0], LightingOnSAL)
+	assert p.sal[0].group_address, 0x21
 
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '053800792129')
-		self.assertEqual(p.confirmation, 'i')
-		
-		# turn off light 0x21
-		p, r = decode_packet('\\0538000121A1k', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], LightingOffSAL)
-		self.assertEqual(p.sal[0].group_address, 0x21)
+	# check that it encodes properly again
+	assert p.encode(), '053800792129'
+	assert p.confirmation, 'i'
+	
+	# turn off light 0x21
+	p, r = decode_packet('\\0538000121A1k', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], LightingOffSAL)
+	assert p.sal[0].group_address == 0x21
 
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '0538000121A1')
-		self.assertEqual(p.confirmation, 'k')
-		
-		# ramp light 0x21 to 50% over 4 seconds
-		p, r = decode_packet('\\0538000A217F19l', server_packet=False)
-		
-		self.assertIsInstance(p, PointToMultipointPacket)
-		self.assertEqual(len(p.sal), 1)
-		
-		self.assertIsInstance(p.sal[0], LightingRampSAL)
-		self.assertEqual(p.sal[0].group_address, 0x21)
-		self.assertEqual(p.sal[0].duration, 4)
-		# rounding must be done to 2 decimal places, as the value isn't actually
-		# 50%, but 49.8039%.  next value is 50.1%.
-		self.assertEqual(round(p.sal[0].level, 2), 0.5)
-		
-		# check that it encodes properly again
-		self.assertEqual(p.encode(), '0538000A217F19')		
-		self.assertEqual(p.confirmation, 'l')
+	# check that it encodes properly again
+	assert p.encode() == '0538000121A1'
+	assert p.confirmation == 'k'
+	
+	# ramp light 0x21 to 50% over 4 seconds
+	p, r = decode_packet('\\0538000A217F19l', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
+	
+	assert isinstance(p.sal[0], LightingRampSAL)
+	assert p.sal[0].group_address == 0x21
+	assert p.sal[0].duration == 4
+	# rounding must be done to 2 decimal places, as the value isn't actually
+	# 50%, but 49.8039%.  next value is 50.1%.
+	assert round(p.sal[0].level, 2) == 0.5
+	
+	# check that it encodes properly again
+	assert p.encode() == '0538000A217F19'		
+	assert p.confirmation == 'l'
 
 
-class EncodeDecodeTests(unittest.TestCase):
-	def runTest(self):
-		"self-made tests of encode then decode"
-		
-		orig = PointToMultipointPacket(application=APP_LIGHTING)
-		orig.source_address = 5
-		orig.sal.append(LightingOnSAL(orig, 27))
-		
-		data = orig.encode()
+def lighting_encode_decode_test():
+	"self-made tests of encode then decode"
+	
+	orig = PointToMultipointPacket(application=APP_LIGHTING)
+	orig.source_address = 5
+	orig.sal.append(LightingOnSAL(orig, 27))
+	
+	data = orig.encode()
 
-		d, r = decode_packet(data)
-		self.assertIsInstance(orig, PointToMultipointPacket)		
-		self.assertEqual(orig.source_address, d.source_address)
-		self.assertEqual(len(orig.sal), len(d.sal))
-		
-		self.assertIsInstance(d.sal[0], LightingOnSAL)
-		self.assertEqual(orig.sal[0].group_address, d.sal[0].group_address)
-		self.assertEqual(r, None)
+	d, r = decode_packet(data)
+	assert isinstance(orig, PointToMultipointPacket)		
+	assert orig.source_address == d.source_address
+	assert len(orig.sal) == len(d.sal)
+	
+	assert isinstance(d.sal[0], LightingOnSAL)
+	assert orig.sal[0].group_address == d.sal[0].group_address
+	
+	# ensure there is no remaining data to be parsed
+	assert r == None
 
 
