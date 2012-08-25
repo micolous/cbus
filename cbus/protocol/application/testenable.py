2ef72a614f2c1835e999763d2e6cfcc4ca78b7ac
Michael Farrell <micolous+git@gmail.com>
2012-08-25 18:49:09 +0930

    Implemented Enable Control (enable) application and some basic tests from the documentation.

--- cbus/protocol/application/testenable.py
+++ cbus/protocol/application/testenable.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# cbus/protocol/application/__init__.py - Applications in CBus
+# cbus/protocol/application/testenable.py - Enable control unit tests
 # Copyright 2012 Michael Farrell <micolous+git@gmail.com>
 # 
 # This library is free software: you can redistribute it and/or modify
@@ -15,18 +15,23 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-from cbus.protocol.application.clock import ClockApplication
-from cbus.protocol.application.enable import EnableApplication
-from cbus.protocol.application.lighting import LightingApplication
-from cbus.protocol.application.temperature import TemperatureApplication
+from cbus.protocol.packet import decode_packet
+from cbus.protocol.pm_packet import PointToMultipointPacket
+from cbus.protocol.application.enable import *
 from cbus.common import *
 
+def S8_11_Test():
+	"Example in enable control application guide, s8.11 (page 7)"
+	# Set the network variable 0x37 to 0x82
+	p, r = decode_packet('\\05CB0002378275g', server_packet=False)
+	
+	assert isinstance(p, PointToMultipointPacket)
+	assert len(p.sal) == 1
 
-APPLICATIONS = {
-	APP_CLOCK: ClockApplication,
-	APP_ENABLE: EnableApplication,
-	APP_LIGHTING: LightingApplication,
-	APP_TEMPERATURE: TemperatureApplication,
-}
-
-
+	assert isinstance(p.sal[0], EnableSetNetworkVariableSAL)
+	assert p.sal[0].variable == 0x37
+	assert p.sal[0].value == 0x82
+	
+	## check that it encodes properly again
+	assert p.encode() == '05CB0002378275'
+	assert p.confirmation == 'g'
