920da59fb442c5d0a739b13d1b0d35ff413f2542
Michael Farrell <micolous+git@gmail.com>
2020-01-02 23:35:23 +1100

    simplify paho_asyncio

--- cbus/paho_asyncio.py
+++ cbus/paho_asyncio.py
@@ -14,38 +14,22 @@ class AsyncioHelper:
         self.misc = None
 
     def on_socket_open(self, client, userdata, sock):
-        print("Socket opened")
-
-        def cb():
-            print("Socket is readable, calling loop_read")
-            client.loop_read()
-
-        self.loop.add_reader(sock, cb)
+        self.loop.add_reader(sock, client.loop_read)
         self.misc = self.loop.create_task(self.misc_loop())
 
     def on_socket_close(self, client, userdata, sock):
-        print("Socket closed")
         self.loop.remove_reader(sock)
         self.misc.cancel()
 
     def on_socket_register_write(self, client, userdata, sock):
-        print("Watching socket for writability.")
-
-        def cb():
-            print("Socket is writable, calling loop_write")
-            client.loop_write()
-
-        self.loop.add_writer(sock, cb)
+        self.loop.add_writer(sock, client.loop_write)
 
     def on_socket_unregister_write(self, client, userdata, sock):
-        print("Stop watching socket for writability.")
         self.loop.remove_writer(sock)
 
     async def misc_loop(self):
-        print("misc_loop started")
         while self.client.loop_misc() == mqtt.MQTT_ERR_SUCCESS:
             try:
                 await asyncio.sleep(1)
             except asyncio.CancelledError:
                 break
-        print("misc_loop finished")
