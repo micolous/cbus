01a7ddfb23145496aa0c1e9c940dfeafa86a053b
CABrouwers <github@charles-andre.com>
2022-04-25 20:37:52 -0400

    Preparation for multi application

--- cbus/common.py
+++ cbus/common.py
@@ -116,9 +116,27 @@ class Application(IntEnum):
     LIGHTING_5d = 0x5d
     LIGHTING_5e = 0x5e
     LIGHTING_LAST = LIGHTING_5f = 0x5f
+    HVACACTUATOR_73 = 0x73
+    HVACACTUATOR_74 = 0x74
+    HVAC = 0xCA
     CLOCK = 0xDF
+    TRIGGER = 0xCA
     ENABLE = 0xCB
     MASTER_APPLICATION = STATUS_REQUEST = 0xff
+
+    def isLighting(val):
+        if isinstance(val,Application):
+          val = int(val)
+        if isinstance(val,int):
+          return int(Application.LIGHTING_FIRST)<=val<=int(Application.LIGHTING_LAST)
+        return False
+
+    def isHvacActuator(val):
+        if isinstance(val,Application):
+          val = int(val)
+        if isinstance(val,int):
+          return int(Application.HVACACTUATOR_73)==val or int(Application.HVACACTUATOR_74)==val
+        return False
 
 
 class CAL(IntEnum):
@@ -429,3 +447,9 @@ def check_ga(group_addr: int) -> None:
         raise ValueError(
             'Group Address out of range ({}..{}), got {}'.format(
                 MIN_GROUP_ADDR, MAX_GROUP_ADDR, group_addr))
+
+
+def check_aa_lighting(app_addr: int | Application) -> None:
+     if not Application.isLighting(app_addr):
+        raise ValueError(
+            'Expected lighting application address, got {}'.format(app_addr))
